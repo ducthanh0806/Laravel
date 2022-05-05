61da2370d8bcd4443b86ee869b6d38519b071316
unknown <ducthah6868@gmail.com>
2022-05-05 15:30:46 +0700

    fix comment

--- database/migrations/2022_05_04_030947_create_categories_table.php
+++ database/migrations/2022_05_04_030947_create_categories_table.php
@@ -17,8 +17,7 @@ class CreateCategoriesTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('description');
-            $table->timestamp('create_at');
-            $table->timestamp('update_at');
+            $table->timestamp();
         });
     }
 
